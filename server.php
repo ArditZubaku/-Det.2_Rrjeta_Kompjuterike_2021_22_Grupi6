8e3e6d70533c547d1dfecfc6cead03c0a47da3cc
ArditZubaku <94994431+ArditZubaku@users.noreply.github.com>
2022-04-10 11:12:15 +0200

    Update server.php

--- server.php
+++ server.php
@@ -20,4 +20,22 @@ class Chat {
     }
 }
 
+do {
+    $accept = socket_accept($socket) or die ("Couldn't accept incoming connection\n");
+    $message = socket_read($accept, 1024) or die ("Couldn't read input");
+    
+    $message = trim($message);
+    echo "Client says:\t".$message."\n\n";
+
+    line = new Chat();
+    echo "Enter reply:\t";
+    $reply = $line->readline();
+
+    socket_write($accept,$reply,strlen($reply)) or die ("Couldn't write output\n");
+}
+
+while(true);
+
+socket_close($accept,$socket);
+
 ?>
