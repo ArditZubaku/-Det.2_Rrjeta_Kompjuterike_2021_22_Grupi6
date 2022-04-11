a76c9ac2af8ea0e7e27829c301a79ded6edb9f98
arianaberisha <94613006+ArianaBrsh@users.noreply.github.com>
2022-04-11 03:24:50 +0200

    Update client.php

--- client.php
+++ client.php
@@ -14,3 +14,19 @@
                     <?php
                     $host="127.0.0.1";
                     $port=20205;
+
+                    
+                     if(isset($_POST['btnSend']))
+                    {
+                        $msg = $_REQUEST['txtMessage'];
+                        $sock = socket_create(AF_INET,SOCK_STREAM,0);
+                        socket_connect($sock,$host,$port);
+
+                        socket_write($sock, $msg ,strlen($msg));
+
+                        $reply = socket_read($sock,1024);
+                        $reply = trim($reply);
+                        $reply = "Server says:\t".$reply;
+
+                    }
+                    ?>
