d87388edea91c0dbd32bfe527df20b5a81003c07
ArditZubaku <94994431+ArditZubaku@users.noreply.github.com>
2022-04-11 02:59:33 +0200

    Update server.php

--- server.php
+++ server.php
@@ -5,13 +5,11 @@ $port = 20205;
 
 set_time_limit(0);
 
-$socket = socket_create(AF_INET, SOCK_STREAM,0) or die ("Couldn't create socket\n");
-$result = socket_bind($socket, $host, $port) or die ("Couldn't bind socket\n");
-
-
-$result = socket_listen($socket,3) or die ("Couldn't set up socket listener\n");
-
+$sock=socket_create(AF_INET,SOCK_STREAM,0);
+socket_bind($sock,$host,$port);
+socket_listen($sock);
 echo "Listening for connections";
+
 
 class Chat {
 
