83ef20248d2d73e2df08d332b6863886f1dd9449
arianaberisha <94613006+ArianaBrsh@users.noreply.github.com>
2022-04-11 03:24:26 +0200

    Update client.php

--- client.php
+++ client.php
@@ -1,3 +1,16 @@
-<?php
+<html>
+    <body>
+        <liv align='center'>
+            <form method='post'>
+                <table>
+                    <tr>
+                        <td>
+                            <label>Enter Message</label>
+                            <input type='text' name='txtMessage'>
+                            <input type='submit' name='btnSend' value='Send'>
 
-?>
+                        </td>
+                    </tr>
+                    <?php
+                    $host="127.0.0.1";
+                    $port=20205;
